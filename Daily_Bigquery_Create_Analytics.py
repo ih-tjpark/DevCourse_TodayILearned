901a847bf3d13d1da0c45c0b5c06d18fc1d18f51
TJ Park <hahxowns@naver.com>
2023-06-30 16:34:56 +0900

    Update Daily_Bigquery_Create_Analytics.py

--- Daily_Bigquery_Create_Analytics.py
+++ Daily_Bigquery_Create_Analytics.py
@@ -86,7 +86,7 @@ with DAG(
         wait_for_completion=False,
         poke_interval=30,
         allowed_states=["success"],
-        
-    )
 
+    )
+asd
     bigquery_create_analytics_table >> trigger
